f1fbf59c6c75095d0495a11dfd6dc71938073dd5
Sarah Kimi Rettig <sarahkimirettig@gmail.com>
2026-02-05 17:55:00 -0500

    x

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -63,7 +63,7 @@ export default async function ProjectsPage() {
   return (
     <div className="relative pb-16">
       <Navigation />
-      <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:pt-24 lg:pt-32">
+      <div className="px-6 pt-20 mx-auto space-y-8 lg:px-8 md:pt-24 lg:pt-32">
         <div className="mx-auto text-center">
           <h2 className="text-3xl font-bold text-center text-black sm:text-4xl">
             Projects
