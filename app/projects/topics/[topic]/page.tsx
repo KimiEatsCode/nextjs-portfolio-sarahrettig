d092b3e63df38c4e7e6a9a38b5496eeaf9395493
Sarah Kimi Rettig <sarahkimirettig@gmail.com>
2026-02-05 15:31:05 -0500

    x

--- app/projects/topics/[topic]/page.tsx
+++ app/projects/topics/[topic]/page.tsx
@@ -76,7 +76,7 @@ export default async function TopicPage({ params }: Props) {
   return (
     <div className="relative pb-16 justify-items-end text-end">
       <Navigation />
-      <div className="px-6 pt-24 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-20 md:pt-20 lg:pt-12">
+      <div className="px-6 pt-24 mx-auto space-y-8 lg:px-8 md:space-y-20 md:pt-20 lg:pt-12">
         <div className="max-w-2xl mx-auto lg:mx-0">
       
           <h2 className="mt-4 text-3xl font-bold tracking-tight text-black sm:text-4xl">
