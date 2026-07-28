c98efa4f9e51cabc3c07683ac53ac2c50dd78dc3
Sarah Kimi Rettig <sarahkimirettig@gmail.com>
2026-07-28 07:23:30 -0400

    update res

--- app/constants.ts
+++ app/constants.ts
@@ -1,5 +1,5 @@
 export const RESUME_URL =
-  "https://docs.google.com/document/d/1YYyn3wMKLyeY3EIq3XT3Qm875kTcx50ZrZah3unNZqo/edit?usp=sharing";
+  "https://drive.google.com/file/d/1K6_KRknV7_2cvAePeZGA8KFhWXybwBK5/view?usp=sharing";
 
   export const CV_RESUME_URL =
   "https://drive.google.com/file/d/1mHrjPevemoRDHBV5dYeLiRL1eWnidxdQ/view?usp=sharing";
